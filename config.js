45b071bfeba52b82121ba9687c6e5fc0f6e0d8a5
shivanijwork <shivanijwork@gmail.com>
2025-09-19 15:25:32 +0530

    chnage

--- config.js
+++ config.js
@@ -1,12 +1,18 @@
-
+require('dotenv').config(); 
 const mongoose = require("mongoose");
+
 mongoose.connect(process.env.MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then((res)=>{
-    console.log("Database Connected")
-}).catch((err)=>{
-    console.log(err)
-});
+   useNewUrlParser: true,
+   serverSelectionTimeoutMS: 15000,
+   autoIndex: false,
+   maxPoolSize: 10,
+   socketTimeoutMS: 45000,
+   family: 4
 
-
+})
+   .then(() => {
+      console.log('MongoDB connected successfully');
+   })
+   .catch((err) => {
+      console.error('MongoDB CONNECTION ERROR =>>: ', err);
+   });
